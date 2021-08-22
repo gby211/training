01b52083bcb73dfb4b918f088a279053c485e529
pinguin <gby211@yandex.tu>
2021-08-22 20:05:00 +0300

    123

---
+++ P0321_SimpleBrowser/src/main/java/com/rtsoft/p0321_simplebrowser/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rtsoft.p0321_simplebrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
