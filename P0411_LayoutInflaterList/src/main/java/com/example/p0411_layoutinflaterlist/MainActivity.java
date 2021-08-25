d6a1444dd6e39313f3004a89ef3a657053cbb1e2
TTawtet <gby211@yandex.ru>
2021-08-25 18:13:00 +0300

    123

--- P0411_LayoutInflaterList/src/main/java/com/example/p0411_layoutinflaterlist/MainActivity.java
+++ P0411_LayoutInflaterList/src/main/java/com/example/p0411_layoutinflaterlist/MainActivity.java
@@ -2,13 +2,61 @@ package com.example.p0411_layoutinflaterlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] name = {"Иван", "Марья", "Петр", "Антон", "Даша", "Борис",
+            "Костя", "Игорь"};
+    String[] position = {"Программер", "Бухгалтер", "Программер",
+            "Программер", "Бухгалтер", "Директор", "Программер", "Охранник"};
+    int salary[] = {13000, 10000, 13000, 13000, 10000, 15000, 13000, 8000};
+
+    int[] colors = new int[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout linLayout = findViewById(R.id.linLayout);
+
+        LayoutInflater ltInflater = getLayoutInflater();
+
+        for (int i = 0; i < 100; i++) {
+
+            Random rnd = new Random();
+            int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+
+            Log.d("myLogs", "i = " + i);
+            View item = ltInflater.inflate(R.layout.item, linLayout, false);
+            TextView tvName = (TextView) item.findViewById(R.id.tvName);
+            tvName.setText(name[i%8]);
+            TextView tvPosition = (TextView) item.findViewById(R.id.tvPosition);
+            tvPosition.setText("Должность: " + position[i%8]);
+            TextView tvSalary = (TextView) item.findViewById(R.id.tvSalary);
+            tvSalary.setText("Оклад: " + String.valueOf(salary[i%8]));
+            item.getLayoutParams().width = LinearLayout.LayoutParams.MATCH_PARENT;
+
+            item.setBackgroundColor(color);
+            color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+            tvSalary.setBackgroundColor(color);
+            color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+            tvPosition.setBackgroundColor(color);
+            color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+            tvName.setBackgroundColor(color);
+
+            linLayout.addView(item);
+
+        }
     }
 }
