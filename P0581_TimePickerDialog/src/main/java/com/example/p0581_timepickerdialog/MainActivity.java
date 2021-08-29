211eebfc9ffc318c5016db3566647ec4efee68a3
TTawtet <gby211@yandex.ru>
2021-08-29 18:24:51 +0300

    123

---
+++ P0581_TimePickerDialog/src/main/java/com/example/p0581_timepickerdialog/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.p0581_timepickerdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
