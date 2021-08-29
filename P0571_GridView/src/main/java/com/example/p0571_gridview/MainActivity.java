b4e3c2201167338266a251705da1e1a2524fcb7f
TTawtet <gby211@yandex.ru>
2021-08-29 17:29:44 +0300

    123

--- P0571_GridView/src/main/java/com/example/p0571_gridview/MainActivity.java
+++ P0571_GridView/src/main/java/com/example/p0571_gridview/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.p0571_gridview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] data = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"};
+
+    GridView gridView;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapter = new ArrayAdapter<String>(this,R.layout.item,R.id.textView, data);
+        gridView = findViewById(R.id.gvMain);
+        gridView.setAdapter(adapter);
+        adjustGridView();
+    }
+
+    private void adjustGridView() {
     }
 }
