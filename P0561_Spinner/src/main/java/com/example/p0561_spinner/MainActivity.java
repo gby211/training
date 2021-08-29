cf06b53d4a4ab988ea866d4ab3859f7d79de931a
TTawtet <gby211@yandex.ru>
2021-08-29 17:17:56 +0300

    123

--- P0561_Spinner/src/main/java/com/example/p0561_spinner/MainActivity.java
+++ P0561_Spinner/src/main/java/com/example/p0561_spinner/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.p0561_spinner;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +19,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String >(this,android.R.layout.simple_spinner_item,data);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        стандартный адаптер и вьюхи
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String >(this,android.R.layout.simple_spinner_item,data);
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spiner_item, R.id.text1, data);
+        Spinner spinner = findViewById(R.id.spinner);
 
 
+        spinner.setAdapter(adapter);
+        // заголовок
+        spinner.setPrompt("Title");
+        // выделяем элемент
+        spinner.setSelection(2);
+        // устанавливаем обработчик нажатия
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), "Position = " + position, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
