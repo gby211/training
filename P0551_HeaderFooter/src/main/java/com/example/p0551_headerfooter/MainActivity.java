c4b83752ec316719ff36308d98232699f0b21be3
TTawtet <gby211@yandex.ru>
2021-08-29 16:20:38 +0300

    123

--- P0551_HeaderFooter/src/main/java/com/example/p0551_headerfooter/MainActivity.java
+++ P0551_HeaderFooter/src/main/java/com/example/p0551_headerfooter/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.HeaderViewListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -39,28 +40,40 @@ public class MainActivity extends AppCompatActivity {
         footer1 = createFooter("footer 1");
         footer2 = createFooter("footer 2");
 
-        fillList();
-
         btn_test =  findViewById(R.id.button1);
 
         onClick(btn_test);
 
+        fillList();
+
+
+
     }
 
     void fillList() {
-        try {
-            lvMain.setAdapter(adapter);
-            lvMain.addHeaderView(header1);
-        }catch (Exception ex) {
-            Log.e(LOG_TAG, ex.getMessage());
-        }
+        lvMain.addHeaderView(header1);
+        lvMain.addHeaderView(header2, "some text for header 2", false);
+        lvMain.addFooterView(footer1);
+        lvMain.addFooterView(footer2, "some text for footer 2", false);
+        lvMain.setAdapter(adapter);
     }
 
     void onClick(View v){
         v.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Object obj;
+                HeaderViewListAdapter hvlAdapter = (HeaderViewListAdapter) lvMain.getAdapter();
+                obj = hvlAdapter.getItem(1);
+                Log.d(LOG_TAG, "hvlAdapter.getItem(1) = " + obj.toString());
+                obj = hvlAdapter.getItem(4);
+                Log.d(LOG_TAG, "hvlAdapter.getItem(4) = " + obj.toString());
 
+                ArrayAdapter<String> alAdapter = (ArrayAdapter<String>) hvlAdapter.getWrappedAdapter();
+                obj = alAdapter.getItem(1);
+                Log.d(LOG_TAG, "alAdapter.getItem(1) = " + obj.toString());
+                obj = alAdapter.getItem(4);
+                Log.d(LOG_TAG, "alAdapter.getItem(4) = " + obj.toString());
             }
         });
     }
@@ -77,4 +90,9 @@ public class MainActivity extends AppCompatActivity {
         return v;
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        finish();
+    }
 }
