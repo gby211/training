64a21ed9f4dc33ba50713095b3960ff614f92ebe
TTawtet <gby211@yandex.ru>
2021-08-30 19:23:14 +0300

    123

---
+++ P0711_PreferencesSimple/src/main/java/com/example/p0711_preferencessimple/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.p0711_preferencessimple;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
