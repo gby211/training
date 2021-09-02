a92d2ba3dcb13e24a80fa893335749ab0486613b
TTawtet <gby211@yandex.ru>
2021-09-02 21:23:26 +0300

    123

---
+++ P1051_FragmentDynamic/src/main/java/com/example/p1051_fragmentdynamic/Fragment1.java
@@ -0,0 +1,67 @@
+package com.example.p1051_fragmentdynamic;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment1 extends Fragment {
+    final String LOG_TAG = "myLogs";
+
+    public Fragment1() {
+        super(R.layout.fragment1);
+    }
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        Log.d(LOG_TAG, "Fragment1 onAttach");
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(LOG_TAG, "Fragment1 onCreate");
+    }
+
+
+
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        Log.d(LOG_TAG, "Fragment1 onActivityCreated");
+    }
+
+    public void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "Fragment1 onStart");
+    }
+
+    public void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "Fragment1 onResume");
+    }
+
+    public void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "Fragment1 onPause");
+    }
+
+    public void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "Fragment1 onStop");
+    }
+
+    public void onDestroyView() {
+        super.onDestroyView();
+        Log.d(LOG_TAG, "Fragment1 onDestroyView");
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "Fragment1 onDestroy");
+    }
+
+    public void onDetach() {
+        super.onDetach();
+        Log.d(LOG_TAG, "Fragment1 onDetach");
+    }
+}
