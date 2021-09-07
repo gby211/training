211eb2f0d8079540d958a3f2a994a5b7220ecafb
TTawtet <gby211@yandex.ru>
2021-09-07 19:37:36 +0300

    123

--- P1091_ListFragment/src/main/java/com/example/p1091_listfragment/MainList.java
+++ P1091_ListFragment/src/main/java/com/example/p1091_listfragment/MainList.java
@@ -1,6 +1,19 @@
 package com.example.p1091_listfragment;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.Nullable;
 import androidx.fragment.app.ListFragment;
 
 public class MainList extends ListFragment {
+    String data[] = new String[]{"one", "two", "three", "four"};
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_list_item_1, data);
+        setListAdapter(adapter);
+    }
 }
