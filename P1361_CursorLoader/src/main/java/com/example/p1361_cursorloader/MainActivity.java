b9a15836b37c32ec2c497c95f38abc1d5be71f44
TTawtet <gby211@yandex.ru>
2021-09-09 19:33:33 +0300

    123

--- P1361_CursorLoader/src/main/java/com/example/p1361_cursorloader/MainActivity.java
+++ P1361_CursorLoader/src/main/java/com/example/p1361_cursorloader/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.p1361_cursorloader;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cursoradapter.widget.SimpleCursorAdapter;
+import androidx.fragment.app.FragmentActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements LoaderManager.LoaderCallbacks {
+
+
+    private static final int CM_DELETE_ID = 1;
+    ListView lvData;
+    DB db;
+    SimpleCursorAdapter scAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // открываем подключение к БД
+        db = new DB(this);
+        db.open();
+
+        // формируем столбцы сопоставления
+        String[] from = new String[] { DB.COLUMN_IMG, DB.COLUMN_TXT };
+        int[] to = new int[] { R.id.ivImg, R.id.tvText };
+
+        // создаем адаптер и настраиваем список
+        scAdapter = new SimpleCursorAdapter(this, R.layout.item, null, from, to, 0);
+        lvData = (ListView) findViewById(R.id.lvData);
+        lvData.setAdapter(scAdapter);
+
+        // добавляем контекстное меню к списку
+        registerForContextMenu(lvData);
+
+        // создаем лоадер для чтения данных
+        LoaderManager.getInstance(this).initLoader(0, null, this);
+    }
+
+    @NonNull
+    @Override
+    public Loader onCreateLoader(int id, @Nullable Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader loader, Object data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader loader) {
+
     }
 }
