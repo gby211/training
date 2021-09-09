405e139520ae1b6e9133ab6bd8836a1b45ac845d
TTawtet <gby211@yandex.ru>
2021-09-09 21:12:45 +0300

    123

--- P1801_constraintlayout/src/main/java/com/example/p1801_constraintlayout/MainActivity.java
+++ P1801_constraintlayout/src/main/java/com/example/p1801_constraintlayout/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.p1801_constraintlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Guideline;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    float tmp = (float) 0.1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Guideline guideline = findViewById(R.id.guideline2);
+                guideline.setGuidelinePercent(tmp);
+                tmp+=0.1;
+                if (tmp >0.9)tmp= (float) 0.1;
+            }
+        });
     }
 }
